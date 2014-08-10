5b474b1e9232d4e73430eddc2d316a8f95ec3514
Kill Face <killface@TheAnnihilatrix.(none)>
2014-08-10 08:01:49 -0400

    initial commit

---
+++ polls/models.py
@@ -0,0 +1,36 @@
+import datetime
+from django.utils import timezone
+from django.db import models
+
+
+# Create your models here.
+
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def is_old(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=90) >= self.pub_date <= now
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    is_old.boolean = True
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+    
+    def __unicode__(self):
+        return self.question
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.choice_text
+
+
